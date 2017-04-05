1c4babd9d2876715b04081fbe9714e97ad32bf24
Rick Rose <Rick Rose@Rick>
2017-04-05 13:58:52 +0300

    Fix

--- Winter Fox Engine/src/com/wfe/terrain/Chunk.java
+++ Winter Fox Engine/src/com/wfe/terrain/Chunk.java
@@ -83,7 +83,7 @@ public class Chunk {
 		VBO = GL15.glGenBuffers();
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBO);
 		FloatBuffer floatBuffer = OpenglUtils.toFloatBuffer(vertices, texCoords, normals);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, floatBuffer, GL15.GL_DYNAMIC_DRAW);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, floatBuffer, GL15.GL_STATIC_DRAW);
 		
 		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, vertexByteCount, 0);
 		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, vertexByteCount, BYTES_PER_FLOAT * 3);
@@ -488,19 +488,10 @@ public class Chunk {
 	}
 	
 	private void rebuild() {
+		cleanup();
+		
 		create();
-		
-		GL30.glBindVertexArray(VAO);
-		
-		// TODO: rewrite
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VBO);
-		FloatBuffer floatBuffer = OpenglUtils.toFloatBuffer(vertices, texCoords, normals);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, floatBuffer, GL15.GL_DYNAMIC_DRAW);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		
-		GL30.glBindVertexArray(0);
-		
-		MemoryUtil.memFree(floatBuffer);
+		initGL();
 	}
 	
 	public void render() {
